0af12f5172bffae6d1964cf5c908be72148a40e1
tupelobound <29886944+tupelobound@users.noreply.github.com>
2023-02-02 20:12:58 +0000

    Added script.js

---
+++ js/script.js
@@ -0,0 +1,19 @@
+// Smooth scrolling on navbar click
+$("#navbar-header a").on('click', function (event) {
+    if (this.hash !== "") {
+        event.preventDefault();
+        var hash = this.hash;
+        $('html, body').animate({
+            scrollTop: $(hash).offset().top
+        }, 1200, function () {
+            window.location.hash = hash;
+        });
+    }
+});
+
+$("#btt").click(function () {
+    $("html, body").animate({
+        scrollTop: 0
+    }, "slow");
+    return false;
+});
